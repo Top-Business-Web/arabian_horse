fa83d9cbb64e171c0cd7ed03748ac6876b856202
abdullah <abdullah.alhumsi9@gmail.com>
2024-01-07 12:55:38 +0200

    add migration

---
+++ database/migrations/2024_01_07_103858_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo')->nullable();
+            $table->string('email');
+            $table->string('phone');
+            $table->longText('address');
+            $table->longText('desc_about');
+            $table->string('link_youtube');
+            $table->string('image');
+            $table->string('instagram');
+            $table->string('linkedin');
+            $table->string('twitter');
+            $table->string('facebook');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
